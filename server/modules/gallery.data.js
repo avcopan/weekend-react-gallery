1a521f1a30d901cd5419d129c10d8fb061d96127
Andreas Copan <avcopan@gmail.com>
2023-06-16 15:20:30 -0400

    Adds images to the database

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,53 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 2,
+    title: "Marriage",
+    path: "images/marriage.jpg",
+    description: "Photo of my wife and me taken before our wedding in Athens, GA.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    title: "Family",
+    path: "images/family1.jpg",
+    description: "Photo of my family taken en route to visit grandparents in Wellington, FL.",
+    likes: 0,
+  },
+  {
+    id: 4,
+    title: "My Girls 1",
+    path: "images/my-girls1.jpg",
+    description: "Photo of my girls taken a year ago in Athens, GA.",
+    likes: 0,
+  },
+  {
+    id: 5,
+    title: "My Girls 2",
+    path: "images/my-girls2.jpg",
+    description: "Photo of my girls taken a few months ago in Athens, GA.",
+    likes: 0,
+  },
+  {
+    id: 6,
+    title: "My Girls 3",
+    path: "images/my-girls3.jpg",
+    description: "Photo of my girls taken en route home from Danvers, MA.",
+    likes: 0,
+  },
+  {
+    id: 7,
+    title: "Working Together",
+    path: "images/working-together.jpg",
+    description: "Photo of my daughter and me washing dishes together in Athens, GA.",
+    likes: 0,
+  },
+  {
+    id: 8,
+    title: "Grandparents",
+    path: "images/grandparents.jpg",
+    description: "Photo of my parents out for a walk in the botanical gardens with my daugher in Athens, GA.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
