cc1190d351db69867a7e4318459702c403e2a7f0
Andreas Copan <avcopan@gmail.com>
2023-06-16 17:54:48 -0400

    Implements one possible image cropping solution
    I would like to be able to shift images to center the content, and I'm
    not sure how to do that with this approach.

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -2,8 +2,8 @@ import "./GalleryItem.css"
 
 export function GalleryItem({ item }) {
   return (
-    <div className="GalleryItem-container">
-      <img className="GalleryItem-image" src={item.path} alt={item.description} />
+    <div className="GalleryItem">
+      <img src={item.path} alt={item.description} />
     </div>
   );
 }
