4dea036cf645f17224a024963422d923e09b57e9
Andreas Copan <avcopan@gmail.com>
2023-06-23 15:31:18 -0400

    Implements toggling between image and description
    If there's time, I would like to have the text overlayed in white on a
    darkened image.

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,9 +1,18 @@
-import "./GalleryItem.css"
+import { useState } from "react";
+import "./GalleryItem.css";
 
 export function GalleryItem({ item }) {
+  const [viewDescription, setViewDescription] = useState(false);
+
+  const toggleViewDescription = () => setViewDescription(!viewDescription);
+
   return (
-    <div className="GalleryItem">
-      <img src={item.path} alt={item.description} />
+    <div className="GalleryItem" onClick={toggleViewDescription}>
+      {viewDescription ? (
+        <div>{item.description}</div>
+      ) : (
+        <img src={item.path} alt={item.description} />
+      )}
     </div>
   );
 }
